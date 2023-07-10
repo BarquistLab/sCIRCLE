5ad3c46cb881b0167224ce0be1fc7580930b38a4
MaxSeeger <46606031+IKnowhere@users.noreply.github.com>
2023-07-10 23:08:27 +0200

    Init

---
+++ UI_scRNA_4.0/UI_scRNA_4_0.Windows/UI_scRNA_4_0App.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace UI_scRNA_4_0
+{
+    class UI_scRNA_4_0App
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
